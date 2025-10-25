921a7fc448f4b957c7e2528c935485f6c73fdf70
TamerPW <tamer666@wp.pl>
2025-10-25 02:02:25 +0200

    Update ResourceManager.hpp

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -1,8 +1,50 @@
 #pragma once
-
 #include "Resource.hpp"
 
 class ResourceManager
 {
-    // Twoja implementacja tutaj
+    public: 
+        ResourceManager()
+        {
+        	cos = new Resource();
+		}
+        
+        double get()
+        {
+        	return cos->get();
+		}
+        
+        ResourceManager(const ResourceManager& i)
+        {
+        	cos = new Resource();
+        	*cos = *(i.cos);
+		}
+        
+    	ResourceManager& operator=(const ResourceManager& i)
+    	{
+    		*cos = *i.cos;
+    		return *this;
+		}
+		
+		ResourceManager(ResourceManager&& i)
+		{
+			cos = (i.cos);
+			i.cos = nullptr;
+		}
+		
+		ResourceManager& operator=(ResourceManager&& i)
+    	{
+    		delete cos;
+    		cos = i.cos;
+    		i.cos = nullptr;
+    		return *this;
+		}
+		
+		~ResourceManager()
+		{
+			delete cos;
+		}
+		
+    private: 
+        Resource* cos; 
 };
